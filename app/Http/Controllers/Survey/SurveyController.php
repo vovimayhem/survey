cf712605cbf4b88e22936921b415c5311f686d10
Anselmo Hernández Bazaldúa <chemo.baza@gmail.com>
2019-01-28 16:48:41 -0600

    CSS group added

--- app/Http/Controllers/Survey/SurveyController.php
+++ app/Http/Controllers/Survey/SurveyController.php
@@ -84,9 +84,7 @@ class SurveyController extends Controller
     $result->status = Result::SURVEY_STATUS_COMPLETED;
     $result->save();
     
-    Notification::route('mail', 'anhernandez@communitytax.com')->notify(new SurveyCompleted($result));
-    Notification::route('mail', 'jdayan@communitytax.com')->notify(new SurveyCompleted($result));
-    Notification::route('mail', 'jseaman@communitytax.com')->notify(new SurveyCompleted($result));
+    Notification::route('mail', 'ccs@communitytax.com')->notify(new SurveyCompleted($result));
 
     return redirect()->route('thanks', [$lang, $case]);
   }
