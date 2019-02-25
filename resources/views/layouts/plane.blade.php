cdfa02c53503876c0133f99b75037aa47389693b
Anselmo Hernández Bazaldúa <chemo.baza@gmail.com>
2019-02-25 11:33:12 -0600

    Tabs, small icons

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -2,10 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<meta content="" name="description" />
+	<meta content="" name="author" />
+
+	<link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon" />    <!-- Favicon -->
+	<link rel="apple-touch-icon-precomposed" href="/img/apple-touch-icon-57-precomposed.png">	<!-- For iPhone -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-touch-icon-114-precomposed.png">    <!-- For iPhone 4 Retina display -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-touch-icon-72-precomposed.png">    <!-- For iPad -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-touch-icon-144-precomposed.png">    <!-- For iPad Retina display -->
 
 	<title>{{ config('app.name', 'Laravel') }}</title>
 	
